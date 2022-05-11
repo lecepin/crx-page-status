753c7959a0b4cf429d9e9a6716ecd62fd8a607ff
Amanda111 <631551773@qq.com>
2022-05-11 14:15:26 +0800

    show resource type

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,3 +1,6 @@
+import { converSize } from './utils.js';
+
+
 (function () {
   const LEVEL_COLOR = {
     NORMAL: "#52c41a",
@@ -18,27 +21,6 @@
 
   const _messageBox = [];
   let _visibleWindow = true;
-
-  function converSize(size) {
-    let _size = "";
-    if (size < 0.1 * 1024) {
-      _size = size.toFixed(2) + "B";
-    } else if (size < 0.1 * 1024 * 1024) {
-      _size = (size / 1024).toFixed(2) + "KB";
-    } else if (size < 0.1 * 1024 * 1024 * 1024) {
-      _size = (size / (1024 * 1024)).toFixed(2) + "MB";
-    } else {
-      _size = (size / (1024 * 1024 * 1024)).toFixed(2) + "GB";
-    }
-
-    const sizestr = _size + "";
-    const len = sizestr.indexOf(".");
-    const dec = sizestr.substr(len + 1, 2);
-    if (dec == "00") {
-      return sizestr.substring(0, len) + sizestr.substr(len + 3, 2);
-    }
-    return sizestr;
-  }
 
   function converTimeSecond(time) {
     const _time = time / 1000;
@@ -401,6 +383,7 @@
                   : "inherit",
               size: converSize(item.transferSize),
               _size: item.transferSize,
+              type: item.initiatorType || 'other'
             };
           }),
         };
